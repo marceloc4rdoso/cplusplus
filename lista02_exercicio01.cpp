5aacf28467d6a15fac6e0241d88eb74c915a038e
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-03-26 19:32:47 -0300

    Create lista02_exercicio01.cpp

---
+++ lista02_exercicio01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//1) Faça um programa que peça um número ao usuário e informe se é Par ou Ímpar.
+
+int main() { 
+  int num;
+  cout << "Digite um número: ";
+  cin >> num;
+  if (num % 2 == 0) {
+    cout << "O número é par";
+  }
+  else{
+    cout << "O número é ímpar";
+  }
+  system("pause");
+}
