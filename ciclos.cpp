fbac8f96a014b35d6f8179f7fac4a9c38de52761
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-03-31 21:23:54 -0300

    Create ciclos.cpp

---
+++ ciclos.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<windows.h>
+
+using namespace std;
+
+
+int main() {
+    
+
+   string senha;
+   cout << "Digite a senha: " << endl;
+   unsigned long long ciclos = 1;
+   
+   while (true){
+       cout << "Número de ciclo: !" << ciclos;
+       sleep(1000); Delay de 1 segundo (milesegundos)
+       ciclos ++;
+   }
+    cout << "Acesso liberado!" << endl;
+    
+    system("pause");
+}
