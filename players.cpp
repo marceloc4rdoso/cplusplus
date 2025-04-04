c83de281d55b9ef34d560f7fbb89a1d1e3d71cf6
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-04-04 18:45:35 -0300

    Create players.cpp

---
+++ players.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string playerOne;
+string playerTwo;
+
+int main() {
+    // Write C++ code here
+   cout << "Insira o nome do 1º jogador: ";
+   getline(cin,playerOne);
+   cout << "Insira o nome do 2º jogador: ";
+   getline(cin,playerTwo);
+   
+   int startPointsPlayOne = 10; 
+   int startPointsPlayTwo = 10;
+
+   cout << "----" << " Starting Points " << "----\n";
+   cout << playerOne <<" | "<< startPointsPlayOne << " X " << startPointsPlayTwo <<" | " << playerTwo << endl;
+   
+   int gameRound = 3
+   
+   for( i )
+   
+   
+}
