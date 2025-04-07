b42933a21c97a5b7c700e34bcd06342529b0eeae
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-04-07 19:59:51 -0300

    Create arrays_begin.cpp

---
+++ arrays_begin.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+    int valores[4]; //Cria um array com 4 letras (indices de 0 a 3)
+int valores [4] = {10,8,5,7} // Cria array com valores definidos
+ 
+int valores [4] = {10,8,5,7} // Cria array com valores definidos
+
+cout << valores [0]Mostrar o valor do indice [0] --> 10
+
+// Número de elementos de um array
+int tamanho = sizeof(valores) / sizeof();
+
+array<int, 4> valores;
+
+
+    
+    
+    
+}
