f0b9d830d38bca8b9808fea987bc5e37e295f4e2
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-04-07 21:53:56 -0300

    Create lista_04_exercicio01.cpp

---
+++ lista_04_exercicio01.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <array> // std::array
+using namespace std;
+
+/*
+1) Para Uma loja registra suas vendas diárias da semana em um array de 7 posições
+(domingo a sábado). Faça um programa que:
+ Peça ao usuário que informe os valores das vendas de cada dia.
+ Calcule o total da semana.
+ Calcule a média de vendas.
+ Informe o dia com maior e menor valor de vendas.
+*/
+
+int main() {
+    
+    array<int, 7> semana = {0};
+    
+}
