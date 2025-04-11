4b03aee087f8acb229e386ee89db8a32a2c38540
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-04-11 19:42:01 -0300

    Create recursividade.cpp

---
+++ recursividade.cpp
@@ -0,0 +1,22 @@
+// para finalizar
+#include <iostream>
+
+using namespace std;
+
+int contador;
+
+void teste (string palavra, int num){
+    cout << "Marcelo <<"; " " << contador << endl;
+    contador++
+    teste(palavra, contador);
+}
+
+
+
+
+int main() {
+    
+    teste("Marcelo", 100);
+    system("pause");
+  
+}
